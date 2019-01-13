0f887b7bf54f614477270098c1ebda91c8ff874e
Alexey <taler2002@gmail.com>
2019-01-13 18:28:19 +0300

    temp

--- src/main/java/com/epam/lab/hospitalspring/HospitalApplication.java
+++ src/main/java/com/epam/lab/hospitalspring/HospitalApplication.java
@@ -7,8 +7,8 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
-@EnableAutoConfiguration
+@SpringBootApplication // включает автоматом, все аннотации, что написаны ниже ))))
+@EnableAutoConfiguration //
 
 // указываем Spring где нужно искать Entity, DAO, Service и т.п.
 @ComponentScan(basePackages = "com.epam.lab.hospitalspring")
