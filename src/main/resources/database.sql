4720830d4ea6ecc803fd5eb4ef0076ab878b3204
Alexey <taler2002@gmail.com>
2019-01-13 17:43:17 +0300

    temp

---
+++ src/main/resources/database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `personal` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `first_name` VARCHAR(255) NOT NULL,
+  `last_name` VARCHAR(255) NOT NULL,
+  `role` VARCHAR(45) NOT NULL,
+  `login` VARCHAR(255) NOT NULL,
+  `password` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`id`))
+;
