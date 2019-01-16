f2ae10f3cc1aae0ef669634b7a6d8b2ec18d5082
Alexey <taler2002@gmail.com>
2019-01-17 01:56:04 +0300

    added menu and some pretty style

--- src/main/java/com/epam/lab/hospitalspring/controller/PatientController.java
+++ src/main/java/com/epam/lab/hospitalspring/controller/PatientController.java
@@ -39,33 +39,6 @@ public class PatientController {
     PrescriptionRepository prescriptionRepository;
 
     LocalDateTime today = LocalDateTime.now();
-
-    // test data
-//    List<Patient> patientList = new ArrayList<Patient>() {
-//        {
-//            add(new Patient(1L, "patient1", "secondName1",  false, true));
-//            add(new Patient(2L, "patient2", "secondName2", false, true, null));
-//            add(new Patient(3L, "patient3", "secondName3", false, true, null));
-//        }
-//    };
-
-//    List<Diagnosis> diagnosisList = new ArrayList<Diagnosis>() {
-//        {
-//            add(new Diagnosis(1L, "dsfsdf", patientList.get(1), true, today));
-//            add(new Diagnosis(2L, "dsfsdf", patientList.get(1), true, today));
-//            add(new Diagnosis(3L, "dsfsdf", patientList.get(1), true, today));
-//            add(new Diagnosis(4L, "dsfsdf", patientList.get(2), true, today));
-//        }
-//    };
-
-//    List<Prescription> prescriptionList = new ArrayList<Prescription>() {
-//        {
-//            add(new Prescription(1L, "description1 !!!", patientList.get(1), diagnosisList.get(1), false, today, PrescriptionType.PROCEDURE));
-//            add(new Prescription(2L, "description2 !!!", patientList.get(1), diagnosisList.get(1), false, today, PrescriptionType.OPERATION));
-//            add(new Prescription(3L, "description3 !!!", patientList.get(2), diagnosisList.get(2), false, today, PrescriptionType.DRUG));
-//        }
-//    };
-
     //list of patients
     @RequestMapping(value = "/patients", method = RequestMethod.GET)
     public String getAllPatients(Model model) {
@@ -74,31 +47,9 @@ public class PatientController {
     }
 
     // Getting for patinet his diagnoises and prescriptions
-    @RequestMapping(value = "/patients/{id}", method = RequestMethod.GET)
+    @RequestMapping(value = "/patientDiagnosisCard/{id}", method = RequestMethod.GET)
     public String getPatient(@PathVariable("id") Long id, Model model) {
-//        model.addAttribute("patient", patientService.getPatientById(id));
-//        model.addAttribute("diagnosisList", diagnosisService.findDiagnosisByPatientId(id));
-//        model.addAttribute("prescriptionList", prescriptionService.findPrescriptionsForDiagnosisByDiagnosisId(id));
+        model.addAttribute("patient", patientService.getPatientById(id));
         return "patientDiagnosisCard";
     }
-
-   // Init base
-//    @GetMapping("/init")
-//    public void addTestData(){
-//        //init
-//        patientList.forEach(patient -> patientRepository.save(patient));
-//        diagnosisList.forEach(diagnosis -> diagnosisRepository.save(diagnosis));
-//        prescriptionList.forEach(prescription -> prescriptionRepository.save(prescription));
-//
-//        // getting all
-//        List<Patient> allPatients = patientService.getAllPatients();
-//        List<Diagnosis> allDiagnosis = diagnosisService.getAllDiagnosis();
-//        List<Prescription> allPrescriptions = prescriptionRepository.findAll();
-//
-//        // sout
-//        allPatients.forEach(System.out::println);
-//        allDiagnosis.forEach(System.out::println);
-//        allPrescriptions.forEach(System.out::println);
-//    }
-
 }
