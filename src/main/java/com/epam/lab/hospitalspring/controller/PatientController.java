647d9113997006c487eb286c5ac4e9d30c5897e4
Alexey <taler2002@gmail.com>
2019-01-17 07:12:23 +0300

    go go

--- src/main/java/com/epam/lab/hospitalspring/controller/PatientController.java
+++ src/main/java/com/epam/lab/hospitalspring/controller/PatientController.java
@@ -1,5 +1,7 @@
 package com.epam.lab.hospitalspring.controller;
 
+import com.epam.lab.hospitalspring.form.PatientForm;
+import com.epam.lab.hospitalspring.model.Patient;
 import com.epam.lab.hospitalspring.repository.DiagnosisRepository;
 import com.epam.lab.hospitalspring.repository.PatientRepository;
 import com.epam.lab.hospitalspring.repository.PrescriptionRepository;
@@ -7,12 +9,10 @@ import com.epam.lab.hospitalspring.service.DiagnosisService;
 import com.epam.lab.hospitalspring.service.PatientService;
 import com.epam.lab.hospitalspring.service.PrescriptionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -33,23 +33,41 @@ public class PatientController {
     PrescriptionRepository prescriptionRepository;
 
     LocalDateTime today = LocalDateTime.now();
+
     //list of patients
-    @RequestMapping(value = "/patients", method = RequestMethod.GET)
+    @GetMapping(value = "/patients")
     public String getAllPatients(Model model) {
         model.addAttribute("patients", patientService.getAllPatients());
         return "patients";
     }
 
     // Getting for patinet his diagnoises and prescriptions
-    @RequestMapping(value = "/patientDiagnosisCard/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/patientDiagnosisCard/{id}")
     public String getPatient(@PathVariable("id") Long id, Model model) {
         model.addAttribute("patient", patientService.getPatientById(id));
         return "patientDiagnosisCard";
     }
 
 
-    @RequestMapping(value = "/addPatient", method = RequestMethod.GET)
-    public String addPatient(@RequestParam("firstName") String firstName, @RequestParam("lastName") String lastName) {
-        return "patients";
+    @GetMapping(value = "/addPatient")
+    public String getAddPatientPage() {
+        return "patient";
+    }
+
+    @PostMapping(value = "/addPatient")
+    public String addPatient(PatientForm patientForm, Model model) {
+
+        Patient patient = patientService.addPatient(patientForm);
+        if (patient != null) {
+            return "redirect:/patients";
+        } else {
+            return "redirect:/error/errorMessage";
+        }
+    }
+
+    @GetMapping(value = "/addPersonal/{message}")
+    public String showError(@PathVariable("message") Long message, Model model) {
+        model.addAttribute("errorMessage", message);
+        return "patient";
     }
 }
--- src/main/java/com/epam/lab/hospitalspring/model/Patient.java
+++ src/main/java/com/epam/lab/hospitalspring/model/Patient.java
@@ -1,25 +1,38 @@
 package com.epam.lab.hospitalspring.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 @Table(name = "patient")
 public class Patient {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
+    @Size(min=1, message = "First name must be at least 1 characters")
     @Column(name = "first_name")
     private String firstName;
+    @Size(min=1, message = "Last name must be at least 1 characters")
     @Column(name = "last_name")
     private String lastName;
     private Boolean discharged;
     private Boolean deleted;
     @OneToMany(mappedBy = "patient", fetch = FetchType.LAZY)
     List<Diagnosis> diagnosisList;
+
+    public Patient(@Size(min = 1, message = "First name must be at least 1 characters") String firstName, @Size(min = 1, message = "Last name must be at least 1 characters") String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
