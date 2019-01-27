d1b821db55eedb725806ee53c34905e89d2f03e3
ColdAsIcee <Jurlique@mail.ru>
2019-01-27 23:45:20 +0300

    add tests

--- src/test/java/com/epam/lab/hospitalspring/controller/PersonalControllerTest.java
+++ src/test/java/com/epam/lab/hospitalspring/controller/PersonalControllerTest.java
@@ -1,60 +1,66 @@
 package com.epam.lab.hospitalspring.controller;
 
-import org.junit.Before;
+import com.epam.lab.hospitalspring.service.PersonalService;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
+
 public class PersonalControllerTest {
 
-    public WebDriver driver;
+    public WebDriver driver = InitRoles.initAdmin();
+
+    @Autowired
+    PersonalService personalService;
+
+    @Test
+    public void testGetPersonals() {
+        InitRoles.initAdmin();
+    }
+
+    @Test
+    public void testGetAddPersonalPage() {
+        driver.findElement(By.id("menuAddPersonal")).click();
+        driver.findElement(By.id("login")).sendKeys("Test@epam.com");
+        driver.findElement(By.id("password")).sendKeys("123456qwe");
+        driver.findElement(By.id("firstname")).sendKeys("Marina");
+        driver.findElement(By.id("lastname")).sendKeys("Avdeeva");
+        driver.findElement(By.id("addpersonal")).click();
+    }
 
     @Test
     public void testUpdatePersonal() {
-        driver = InitRoles.initAdmin();
-        System.out.println("Page title is: " + driver.getTitle());
+        WebElement personalTable = driver.findElement(By.id("example"));
+        personalTable.findElements(By.tagName("tr")).get(10).click();
+        driver.findElement(By.id("openprofile")).click();
+        driver.findElement(By.id("field5")).clear();
+        driver.findElement(By.id("field5")).sendKeys("Marina@epam.com");
+        driver.findElement(By.id("password")).clear();
+        driver.findElement(By.id("password")).sendKeys("123456qwe");
+        driver.findElement(By.id("field1")).clear();
+        driver.findElement(By.id("field1")).sendKeys("Marina123");
+        driver.findElement(By.id("field2")).clear();
+        driver.findElement(By.id("field2")).sendKeys("Avdeeva123");
+        driver.findElement(By.id("saveprofile")).click();
+    }
 
+    @Test
+    public void testGetPersonalPage() {
+        WebElement personalTable = driver.findElement(By.id("example"));
+        personalTable.findElements(By.tagName("tr")).get(10).click();
+        driver.findElement(By.id("openprofile")).click();
+    }
+
+    @Test
+    public void testDeletePersonalFromDB() {
         WebElement personalTable = driver.findElement(By.id("example"));
         List<WebElement> rows = personalTable.findElements(By.tagName("tr"));
         rows.get(1).click();
-
-        driver.findElement(By.id("openprofile")).click();
-
-//        <--updatePersonal-->
-        driver.findElement(By.id("field5")).clear();
-        driver.findElement(By.id("field5")).sendKeys("Marina@epam.com");
-
-        driver.findElement(By.id("password")).clear();
-        driver.findElement(By.id("password")).sendKeys("123456qwe");
-
-        driver.findElement(By.id("field1")).clear();
-        driver.findElement(By.id("field1")).sendKeys("Marina");
-
-        driver.findElement(By.id("field2")).clear();
-        driver.findElement(By.id("field2")).sendKeys("Avdeeva");
-
-        driver.findElement(By.id("saveprofile")).click();
-
-////       <--deleted-->
-//        WebElement personalTable2 = driver.findElement(By.id("example"));
-//        List<WebElement> rows2 = personalTable2.findElements(By.tagName("tr"));
-//        rows2.get(4).click();
-//
-//        driver.findElement(By.id("buttondel")).submit();
-////        <--exit-->
-//        driver.findElement(By.id("exit")).submit();
+        driver.findElement(By.id("deletepersonalDB")).click();
     }
 
-//@Test
-//public void testDeletePersonalFromDB(){
-//    driver.findElement(By.id("personalDeleted")).submit();
-//}
-//    @Test
-//    public void testExitFromAdmin() {
-//        driver.findElement(By.id("exit")).submit();
-//    }
 }
