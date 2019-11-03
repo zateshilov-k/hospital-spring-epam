18cbde95435272483e186758d7aafc5b831f603b
Zateshilov Konstantin <headon1243@gmail.com>
2019-11-03 14:21:43 +0300

    replace server port

--- src/main/resources/db/migration/V1__Init_DB.sql
+++ src/main/resources/db/migration/V1__Init_DB.sql
@@ -7,7 +7,7 @@ create table diagnosis
   time        datetime     null,
   patient_id  bigint       not null,
   personal_id bigint       not null
-);
+) CHARACTER SET=utf8mb4;
 
 create index FK8qmvjwaxcj6gjo9evce5uxs4f
   on diagnosis (personal_id);
@@ -18,7 +18,7 @@ create index FKp8tgyroh9ehqikufxe905q0xs
 create table hibernate_sequence
 (
   next_val bigint null
-);
+) CHARACTER SET=utf8mb4;
 
 create table patient
 (
@@ -28,7 +28,7 @@ create table patient
   discharged bit          null,
   first_name varchar(255) null,
   last_name  varchar(255) null
-);
+)CHARACTER SET=utf8mb4;
 
 create table personal
 (
@@ -40,7 +40,7 @@ create table personal
   login      varchar(255) null,
   password   varchar(255) null,
   role       varchar(255) null
-);
+) CHARACTER SET=utf8mb4;
 
 create table prescription
 (
@@ -51,7 +51,7 @@ create table prescription
   time         datetime     null,
   type         varchar(255) null,
   diagnosis_id bigint       not null
-);
+) CHARACTER SET=utf8mb4;
 
 create index FKrj69paft3na7qqssd01vm9mtp
   on prescription (diagnosis_id);
